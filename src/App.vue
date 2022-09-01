213649a1292b5ab450ff55c11a0e15cd9e7bcc5f
Diana Jurca <diana.roxana.lazar@gmail.com>
2022-09-01 18:58:15 +0200

    Iteration 1,2,4 almost ready

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,56 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>IronContacts</h1>
+  <div class="btns">
+    <button @click="randomActor">Add Random Contact</button>
+    <button @click="mostPopular">Sort by Popularity</button>
+    <button @click="alphaSort">Sort by Name</button>
+
+  </div>
+  <section class="head">
+    <h2>Picture</h2>
+    <h2>Name</h2>
+    <h2>Popularity</h2>
+    <h2>Won Oscar</h2>
+    <h2>Won Emmy</h2>
+    <h2>Actions</h2>
+  </section>
+  
+  <div class="contact" v-for="contact in firstFive" :key="contact.id">
+    <img :src="contact.pictureUrl" alt='actor'/>
+    <div>{{contact.name}}</div>
+    <div>{{contact.popularity}}</div>
+    <div v-if="contact.wonOscar">Won Oscar</div>
+    <div v-else></div>
+    <div v-if="contact.wonEmmy">Won Emmy</div>
+    <div v-else></div>
+  </div>
+
 </template>
+
+<script>  
+import contacts from "./contacts.json";
+const actors = contacts;
+const firstFive = actors.slice(0,5);
+
+export default {
+  name: "App",
+  methods: {
+     randomActor() {
+      let randomActorList = contacts.splice(5, contacts.length - 1);
+      this.firstFive.unshift(randomActorList[0]);
+    },
+    mostPopular() {
+      this.firstFive.sort((a,b) => a.popularity > b.popularity);
+    },
+    alphaSort() {
+      this.firstFive.sort((a,b) => a.name > b.name);
+    } 
+
+  }
+}
+</script>
+
 
 <style>
 #app {
